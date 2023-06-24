2f7eb22e08742da6a3f250b31f480fe9a5a824b5
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-06-25 01:42:51 +0300

    Css Changes...

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,32 +6,32 @@ const DUMMY_DATA = [
   {
     id: 1,
     todo: "Learn React",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
   {
     id: 2,
     todo: "Learn Redux",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
   {
     id: 3,
     todo: "Learn Nodejs",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
   {
     id: 4,
     todo: "Learn Express.js",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
   {
     id: 5,
     todo: "Learn MongoDB",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
   {
     id: 6,
     todo: "Learn Redux Saga",
-    date: "16:01:55 24.06.2023",
+    date: "16:01:55 / 24.06.2023",
   },
 ];
 
